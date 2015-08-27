4fb658834db96136da34c46d99ea06e6f958e710
Sam Chiu(邱炫儒) <sam_chiu@htc.com>
2015-08-27 18:41:04 +0800

    add code

---
+++ app/src/main/java/com/samtest/myandroidapp/CONFIG.java
@@ -0,0 +1,10 @@
+package com.samtest.myandroidapp;
+
+/**
+ * Created by mac on 15/8/11.
+ */
+public class CONFIG {
+
+    public final static boolean DEBUG = false;
+
+}
---
+++ app/src/main/java/com/samtest/myandroidapp/Util.java
@@ -0,0 +1,7 @@
+package com.samtest.myandroidapp;
+
+/**
+ * Created by mac on 15/8/11.
+ */
+public class Util {
+}
